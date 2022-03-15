02fa6771f8ff6aee3c9ec8c802625aa60b5b754a
bsspencer42 <mk8golfr2022@gmail.com>
2022-03-14 21:25:29 -0400

    WorldEditor tile change feature add

---
+++ src/main/java/helpz/LoadSave.java
@@ -0,0 +1,15 @@
+package helpz;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class LoadSave {
+
+    public static void CreateFile(){
+
+        File txtFile = new File("/testTextFile.txt");
+
+
+    }
+
+}
